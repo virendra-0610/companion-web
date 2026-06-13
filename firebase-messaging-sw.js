c717188257621f45430a5641be05251b123194f7
Virendra <virendra.dalal@hotmail.com>
2026-06-14 01:59:37 +0530

    PATCH#2 : Resolving notifications issue
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCvgQk98s3JIgTNgVDd3zNqBee8YbEep4s",
+  authDomain: "companion-54a37.firebaseapp.com",
+  projectId: "companion-54a37",
+  storageBucket: "companion-54a37.firebasestorage.app",
+  messagingSenderId: "912271580403",
+  appId: "1:912271580403:web:2b439e27daf3d82baf6b86"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle =
+    payload.notification?.title || 'Companion';
+
+  const notificationOptions = {
+    body: payload.notification?.body || '',
+    icon: '/companion-web/icons/Icon-192.png',
+    badge: '/companion-web/icons/Icon-192.png',
+    data: payload.data || {}
+  };
+
+  self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
