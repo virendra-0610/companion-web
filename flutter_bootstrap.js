78f0bc85045ea6b338c2e899a1654263b79473e3
Virendra <virendra.dalal@hotmail.com>
2026-06-14 01:32:22 +0530

    Added firebase support to enable push notifications
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3705128431" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1182538697" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
