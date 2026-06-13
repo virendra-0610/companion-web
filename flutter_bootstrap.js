e3d80a22bd10e70e95c7b30d9bf6e8e527e3cf06
Virendra <virendra.dalal@hotmail.com>
2026-06-13 23:33:24 +0530

    Revert "Adding notification alerts for weather and to-do lists"
    This reverts commit 3b56d629f25c0f6650cd992133247de310496fd6.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2732383218" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2204903011" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
