8625b19c70bfe8f330c4503809f44c664b427330
Virendra <virendra.dalal@hotmail.com>
2026-06-21 22:49:43 +0530

    Improved weather cards
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1557949783" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2185411416" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
