30d916f8a5f3e5b896562ab5a2bce8005da340a2
Virendra <virendra.dalal@hotmail.com>
2026-06-21 23:17:31 +0530

    Sprint3I modifications
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2185411416" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2068961390" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
