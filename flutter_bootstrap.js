e5520077281898b9c052a2343e03896cb8ea7902
Virendra <virendra.dalal@hotmail.com>
2026-06-21 20:05:16 +0530

    Re-structring the home card
    -> Improvements on weather cards
    -> Adding moon and tithi cards
    
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1629424413" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "96223369" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
