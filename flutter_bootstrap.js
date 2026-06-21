19cc35a1e718933e6a2456422ac7d7aaec74f065
Virendra <virendra.dalal@hotmail.com>
2026-06-21 20:27:15 +0530

    Improving the home card
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "96223369" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1557949783" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
