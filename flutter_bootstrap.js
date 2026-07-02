052257cc390c8e86cd3e317fe096afaf9fae112a
Virendra <virendra.dalal@hotmail.com>
2026-07-03 00:22:37 +0530

    Improvement in notification history
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1926640625" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1712214334" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
