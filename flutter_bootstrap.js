26134aa62ae124f4404aee9140c8a1ff4fe394ae
Virendra <virendra.dalal@hotmail.com>
2026-07-05 01:27:32 +0530

    Added travel advice card
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "792598305" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1819034446" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
