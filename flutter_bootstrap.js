d1ac680f58d48075ff430715a782f8dd5cf6484f
Virendra <virendra.dalal@hotmail.com>
2026-07-05 00:37:09 +0530

    Fixing discover cards
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3470129233" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3545090921" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
