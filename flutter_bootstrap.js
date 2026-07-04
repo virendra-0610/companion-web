4a555baddb018f4b44bf230f79245a21ccef7877
Virendra <virendra.dalal@hotmail.com>
2026-07-04 14:18:51 +0530

    Added dynamic emergency contacts
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3268103148" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4025587945" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
