4e9b1c43cc8fe7157ca21d919800e22df55d2dbc
Virendra <virendra.dalal@hotmail.com>
2026-07-06 00:43:31 +0530

    Vercel migration completed
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2655768093" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3719900767" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
