c5f9cf04753c5d97a18c84a4f3fd04fa72af0243
Virendra <virendra.dalal@hotmail.com>
2026-07-06 23:57:02 +0530

    Frontend v12 daily brief and location normalization
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2531030350" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2130763274" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
