e33a429770f80d050bbf32d429cd249111606a55
Virendra <virendra.dalal@hotmail.com>
2026-07-08 00:43:11 +0530

    PATCH#2: Updating Home and notification UI
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3195589009" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2294553578" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
