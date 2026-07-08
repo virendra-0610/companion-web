c8be0eccb73ab956dba983a22d9988bb470bcb80
Virendra <virendra.dalal@hotmail.com>
2026-07-09 00:39:18 +0530

    Major UI changes - Part 3
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1884235498" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3836244008" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
