1d6b80d99953fded0e3bf216a09b2d2270134f65
Virendra <virendra.dalal@hotmail.com>
2026-07-25 01:20:44 +0530

    v35: Adding high quality animation in weather
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4109528184" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "556456811" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
