48f458f701a173324c3df8e6d6c5a3433a41d7bc
Virendra <virendra.dalal@hotmail.com>
2026-07-24 23:58:29 +0530

    v34: UI updates in weather
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "237297385" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4109528184" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
