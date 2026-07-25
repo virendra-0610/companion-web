5e6dc932a15604c4f6b4d66c45ab2b4b5a96a79e
Virendra <virendra.dalal@hotmail.com>
2026-07-26 02:15:34 +0530

    Adding AI CONCIERGE mode
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2403163193" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3193812456" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
