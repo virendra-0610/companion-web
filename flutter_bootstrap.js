00f076a72493948cf3e77f5b27311a634b63172d
Virendra <virendra.dalal@hotmail.com>
2026-07-25 23:43:36 +0530

    v53: Improvements in overall app design
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3780506404" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "628275006" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
