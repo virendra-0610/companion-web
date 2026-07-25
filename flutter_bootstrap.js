a29aa075b1f72391f152c08fa54200b4bb525273
Virendra <virendra.dalal@hotmail.com>
2026-07-26 01:34:18 +0530

    Adding chatGPT integration with companion
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2520979331" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2403163193" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
