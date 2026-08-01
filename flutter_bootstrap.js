055951926b11085cf37fe4ddb29b9bf74643acde
Virendra <virendra.dalal@hotmail.com>
2026-08-01 15:36:59 +0530

    v85: Fixing issues in last build
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3931557499" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1563555300" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
