3606c7a46ac3dad30ef9c426d96d455745a48e7e
Virendra <virendra.dalal@hotmail.com>
2026-08-02 00:12:54 +0530

    Fixing JSON file issues
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "168179795" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "711816760" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
