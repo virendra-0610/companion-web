4651c5e33bb59f541a8cfcf643b21e757d9fe4f1
Virendra <virendra.dalal@hotmail.com>
2026-08-03 01:00:07 +0530

    v87: Improving Explore section
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "711816760" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "584809347" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
