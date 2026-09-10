928dff789ec18c8bf0e1d8ddedd7c3d07879fba6
Virendra <virendra.dalal@hotmail.com>
2026-09-10 23:51:30 +0530

    v89: Fixed discover and Know latvia section
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1261068217" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3461418405" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
