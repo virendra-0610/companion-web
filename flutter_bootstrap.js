7d20faf7b2b08b466b3b5193a4d82f9dd61ca2a4
Virendra <virendra.dalal@hotmail.com>
2026-09-14 17:32:47 +0530

    v94: Extending Expense tracker
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1272661387" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "564988835" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
