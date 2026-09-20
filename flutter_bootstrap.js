fece5177413fd2ced0c1ad722510a5a6effb051a
Virendra <virendra.dalal@hotmail.com>
2026-09-20 15:08:51 +0530

    v98: Improved bill scanner
    Signed-off-by: Virendra <virendra.dalal@hotmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4231741254" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1265478278" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
